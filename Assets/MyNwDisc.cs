4dba89b8fadc6e94e7fbc84970429293baa72899
Magnus Wamby <wamby@student.chalmers.se>
2019-04-01 01:15:46 +0200

    :^)

---
+++ Assets/MyNwDisc.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNwDisc : NetworkDiscovery
+{
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        base.OnReceivedBroadcast(fromAddress, data);
+        Debug.Log($"Hi! {fromAddress} sending {data}");
+    }
+}
